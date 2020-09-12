c856c384424f55b0aa420a303a3e58c21c0f612e
Daniel McCrystal <d.mccrystal512@gmail.com>
2020-09-12 00:32:18 -0400

    MIDI integration and key colors

--- chord-sight/src/Keyboard.js
+++ chord-sight/src/Keyboard.js
@@ -1,71 +1,174 @@
+import MidiManager from './MidiManager';
 import React from 'react';
+
+const NUM_KEYS = 84;
+
+const WHITE_KEY_WIDTH = 28;
+const WHITE_KEY_HEIGHT = 115;
+
+const BLACK_KEY_WIDTH = 18;
+const BLACK_KEY_HEIGHT = 78;
+
+const WHITE_KEY_PATTERN = [true, false, true, false, true, true, false, true, false, true, false, true];
+const NOTE_NAMES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+
+const KEY_SATURATION_RANGE = [20, 80];
+const KEY_LIGHTNESS_RANGE = [20, 80]
+
+class KeyboardKey extends React.Component {
+
+	state = {
+		keyActive: false,
+		velocity: 0,
+	}
+
+	constructor(props) {
+		super(props);
+
+		this.keyColor = this.getKeyColor();
+	}
+
+	enableKey(velocity) {
+		this.setState({keyActive: true, velocity: velocity});
+	}
+
+	disableKey() {
+		this.setState({keyActive: false});
+	}
+
+	getKeyColor(velocity) {
+		let hue = Math.floor(((this.props.index % 12) / 12) * 360)
+
+		let velocityMultiplier = velocity / 127;
+		let lightness = Math.floor(KEY_LIGHTNESS_RANGE[1] - (velocityMultiplier * (KEY_LIGHTNESS_RANGE[1] - KEY_LIGHTNESS_RANGE[0])));
+		let saturation = Math.floor(KEY_SATURATION_RANGE[0] + (velocityMultiplier * (KEY_SATURATION_RANGE[1] - KEY_SATURATION_RANGE[0])));
+
+		return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+	}
+
+	getWhiteKeyStyle() {
+		return {
+			height: WHITE_KEY_HEIGHT,
+			width: WHITE_KEY_WIDTH,
+			outline: '1px solid black',
+			borderRadius: '2px',
+
+			backgroundColor: this.state.keyActive ? this.getKeyColor(this.state.velocity) : 'whitesmoke',
+
+			display: 'flex',
+			justifyContent: 'center',
+		};
+	}
+
+	getBlackKeyStyle(numKeysOffset) {
+
+		let pxOffset = (numKeysOffset * WHITE_KEY_WIDTH) - (BLACK_KEY_WIDTH / 2);
+
+		return {
+			height: BLACK_KEY_HEIGHT, 
+			width: BLACK_KEY_WIDTH, 
+
+			position: 'absolute',
+			left: pxOffset,
+
+			borderRadius: '2px', 
+
+			outline: '1px solid black',
+			backgroundColor: this.state.keyActive ? this.getKeyColor(this.state.velocity) : 'black',
+
+			display: 'flex',
+			justifyContent: 'center',
+		}
+	}
+
+	render() {
+		return <div style={this.props.white ? this.getWhiteKeyStyle() : this.getBlackKeyStyle(this.props.numKeysOffset)} onClick={() => this.toggleKey()}>
+			
+			<div 
+				style={{
+					alignSelf: 'flex-end', 
+					marginBottom: '8px', 
+					width: '15px', 
+					height: '15px', 
+					borderRadius: '15px', 
+					backgroundColor: 'white', 
+					fontSize: 10, 
+					display: this.state.keyActive ? 'flex' : 'none',
+					justifyContent: 'center', 
+					alignItems: 'center',
+				}}
+			>
+				{NOTE_NAMES[this.props.index % 12]}
+			</div>
+		</div>
+	}
+}
 
 export default class Keyboard extends React.Component {
 
-	NUM_KEYS = 84;
-
-	WHITE_KEY_WIDTH = 28;
-	WHITE_KEY_HEIGHT = 115;
-
-	BLACK_KEY_WIDTH = 18;
-	BLACK_KEY_HEIGHT = 78;
 	
-	WHITE_KEY_PATTERN = [true, false, true, false, true, true, false, true, false, true, false, true];
-
 	keyRefs = [];
 
 	constructor(props) {
 		super(props);
-		for (let i=0; i<this.NUM_KEYS; i++) {
+		for (let i=0; i<NUM_KEYS; i++) {
 			this.keyRefs.push(React.createRef());
+		}
+
+	}
+
+	componentDidMount() {
+		MidiManager.setUpMidi();
+		MidiManager.listenForMidiEvents((event) => this.onMidiEvent(event));
+	}
+
+	onMidiEvent(event) {
+		let [eventType, noteIndex, velocity] = event;
+		console.log(eventType);
+
+		if (eventType === 144) { // note on
+			console.log(event)
+			if (noteIndex >= 24 && noteIndex < NUM_KEYS + 24) {
+				this.keyRefs[noteIndex - 24].current.enableKey(velocity);
+			} else {
+				console.log(`Note out of range: ${noteIndex}`);
+			}
+		} else if (eventType === 128) { // note off
+			if (noteIndex >= 24 && noteIndex < NUM_KEYS + 24) {
+				this.keyRefs[noteIndex - 24].current.disableKey();
+			} else {
+				console.log(`Note out of range: ${noteIndex}`);
+			}
+		} else {
+			console.log("Unhandled MIDI operation")
 		}
 	}
 
-	getWhiteKey(index) {
-		return <div 
-			style={{
-				height: this.WHITE_KEY_HEIGHT,
-				width: this.WHITE_KEY_WIDTH,
-				outline: '1px solid black',
-				borderRadius: '2px',
-				backgroundColor: 'whitesmoke'
-			}} 
-			key={index}
-			ref={this.keyRefs[index]}
-		/>;
-	}
-
-	getBlackKey(index, numKeysOffset) {
-
-		let pxOffset = (numKeysOffset * this.WHITE_KEY_WIDTH) - (this.BLACK_KEY_WIDTH / 2);
-
-		return <div 
-			style={{
-				height: this.BLACK_KEY_HEIGHT, 
-				width: this.BLACK_KEY_WIDTH, 
-
-				position: 'absolute',
-				left: pxOffset,
-
-				borderRadius: '2px', 
-				backgroundColor: 'black', 
-			}}
-			key={index}
-			ref={this.keyRefs[index]}
-		/>
-	}
+	
 
 	getKeyArray() {
 		let keys = [];
 		let numWhiteKeys = 0;
 		
-		for (let i=0; i<this.NUM_KEYS; i++) {
+		for (let i=0; i<NUM_KEYS; i++) {
 			let key;
-			if (this.WHITE_KEY_PATTERN[i % 12]) {
-				key = this.getWhiteKey(i);
+			if (WHITE_KEY_PATTERN[i % 12]) {
+				key = <KeyboardKey 
+					index={i+24} 
+					white 
+					key={i}
+					ref={this.keyRefs[i]}
+				/>
 				numWhiteKeys++;
 			} else {
-				key = this.getBlackKey(i, numWhiteKeys);
+				key = <KeyboardKey 
+					index={i+24} 
+					white={false} 
+					numKeysOffset={numWhiteKeys}
+
+					key={i}
+					ref={this.keyRefs[i]}
+				/>
 			}
 			
 			keys.push(key);
---
+++ chord-sight/src/MidiManager.js
@@ -0,0 +1,47 @@
+
+
+export default class MidiManager {
+
+	static listeners = [];
+
+	static setUpMidi() {
+		navigator.requestMIDIAccess().then(MidiManager.onMidiSucess, MidiManager.onMidiFail);
+	}
+
+	static onMidiSucess(midiAcess) {
+		console.log(midiAcess);
+
+		let inputs = midiAcess.inputs;
+
+		if (inputs) {
+			for (let input of inputs.values()) {
+				input.onmidimessage = MidiManager.midiEventOccurred;
+			}
+		}
+	}
+
+	static onMidiFail() {
+		console.log("Failed to connect MIDI");
+	}
+
+	static listenForMidiEvents(callback) {
+		MidiManager.listeners.push(callback);
+	}
+
+	static midiEventOccurred(event) {
+		MidiManager.listeners.forEach(callback => callback(event.data));
+	}
+	
+	static checkMidiAccess() {
+		console.log('Checking MIDI Access');
+		console.log(navigator);
+		if (navigator.requestMIDIAccess) {
+			console.log('This browser supports WebMIDI!');
+			return true;
+		} else {
+			console.log('WebMIDI is not supported in this browser.');
+			return false;
+		}
+	}
+
+}
