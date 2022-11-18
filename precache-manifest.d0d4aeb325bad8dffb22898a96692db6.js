c0f2a793bb6dcc7ac07f1430307b8aa5b6fe0650
Daniel McCrystal <d.mccrystal512@gmail.com>
2022-11-17 21:45:20 -0500

    Updates

---
+++ precache-manifest.d0d4aeb325bad8dffb22898a96692db6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce55f3b59fd3278ffb3a2923bedc432b",
+    "url": "/ChordSight/index.html"
+  },
+  {
+    "revision": "7fc33f6939409461965f",
+    "url": "/ChordSight/static/css/main.751ab14c.chunk.css"
+  },
+  {
+    "revision": "1ee8e4a9747489ec3d04",
+    "url": "/ChordSight/static/js/2.796ea54a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ChordSight/static/js/2.796ea54a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fc33f6939409461965f",
+    "url": "/ChordSight/static/js/main.4f50615a.chunk.js"
+  },
+  {
+    "revision": "c0d8b2d8266e6bc9cac3",
+    "url": "/ChordSight/static/js/runtime-main.12992220.js"
+  }
+]);
